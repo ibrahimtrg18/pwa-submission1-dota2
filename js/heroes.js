b962222c63873950673e31da0da05c6697907443
ibrahim <ibrahimtarigan@gmail.com>
2020-08-20 00:04:14 +0700

    heroes by side

--- js/heroes.js
+++ js/heroes.js
@@ -1,8 +1,8 @@
+// hero radiant strength
 fetch('/data.json').then(res => res.json()).then(data => {
-    console.log(data.data.heroes)
     for (var i = 0; i < data.data.heroes.length; i++) {
-        if (data.data.heroes[i].int_main_attributes === 1)
-            document.getElementById("heroes-strength-content").innerHTML += `
+        if (data.data.heroes[i].int_main_attributes === 1 && data.data.heroes[i].side === "radiant")
+            document.getElementById("heroes-radiant-strength-content").innerHTML += `
                 <div class="col s6 m4 heroes">
                     <div class="heroes-image-container">
                         <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
@@ -12,11 +12,25 @@ fetch('/data.json').then(res => res.json()).then(data => {
     }
 })
 
+// hero dire strength
 fetch('/data.json').then(res => res.json()).then(data => {
-    console.log(data.data.heroes)
     for (var i = 0; i < data.data.heroes.length; i++) {
-        if (data.data.heroes[i].int_main_attributes === 2)
-            document.getElementById("heroes-agility-content").innerHTML += `
+        if (data.data.heroes[i].int_main_attributes === 1 && data.data.heroes[i].side === "dire")
+            document.getElementById("heroes-dire-strength-content").innerHTML += `
+                <div class="col s6 m4 heroes">
+                    <div class="heroes-image-container">
+                        <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
+                        <span>${data.data.heroes[i].name}</span>
+                    </div>
+                </div>`
+    }
+})
+
+// hero radiant agility
+fetch('/data.json').then(res => res.json()).then(data => {
+    for (var i = 0; i < data.data.heroes.length; i++) {
+        if (data.data.heroes[i].int_main_attributes === 2 && data.data.heroes[i].side === "radiant")
+            document.getElementById("heroes-radiant-agility-content").innerHTML += `
             <div class="col s6 m4 heroes">
                 <div class="heroes-image-container ">
                     <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
@@ -26,11 +40,41 @@ fetch('/data.json').then(res => res.json()).then(data => {
     }
 });
 
+// hero dire agility
+fetch('/data.json').then(res => res.json()).then(data => {
+    for (var i = 0; i < data.data.heroes.length; i++) {
+        if (data.data.heroes[i].int_main_attributes === 2 && data.data.heroes[i].side === "dire")
+            document.getElementById("heroes-dire-agility-content").innerHTML += `
+                <div class="col s6 m4 heroes">
+                    <div class="heroes-image-container">
+                        <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
+                        <span>${data.data.heroes[i].name}</span>
+                    </div>
+                </div>`
+    }
+})
+
+// hero radiant intelligence
 fetch('/data.json').then(res => res.json()).then(data => {
     console.log(data.data.heroes)
     for (var i = 0; i < data.data.heroes.length; i++) {
-        if (data.data.heroes[i].int_main_attributes === 3)
-            document.getElementById("heroes-intelligence-content").innerHTML += `
+        if (data.data.heroes[i].int_main_attributes === 3 && data.data.heroes[i].side === "radiant")
+            document.getElementById("heroes-radiant-intelligence-content").innerHTML += `
+                <div class="col s6 m4 heroes">
+                    <div class="heroes-image-container">
+                        <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
+                        <span>${data.data.heroes[i].name}</span>
+                    </div>
+                </div>`
+    }
+})
+
+// hero dire intelligence
+fetch('/data.json').then(res => res.json()).then(data => {
+    console.log(data.data.heroes)
+    for (var i = 0; i < data.data.heroes.length; i++) {
+        if (data.data.heroes[i].int_main_attributes === 3 && data.data.heroes[i].side === "dire")
+            document.getElementById("heroes-dire-intelligence-content").innerHTML += `
             <div class="col s6 m4 heroes">
                 <div class="heroes-image-container ">
                     <img src="${data.data.heroes[i].path_image}" width="100%" height="auto" />
